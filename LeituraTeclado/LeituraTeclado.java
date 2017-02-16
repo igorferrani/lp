281ebbfe99f9d66b7c5c8730b4e6fcb711bc8664
felipefo <felipefo@gmail.com>
2017-02-16 09:49:29 -0200

    .
    .

--- LeituraTeclado/LeituraTeclado.java
+++ LeituraTeclado/LeituraTeclado.java
@@ -1,20 +1,17 @@
-import javax.swing.*;
 
+import javax.swing.*;
 public class LeituraTeclado
 {
-
     public void  leitura()
     {
         String resposta="";
         float nota1=0;float nota2=0;float media=0;   
-
         resposta = JOptionPane.showInputDialog("Entre com primeira nota: ");
-        nota1 = Float.parseFloat(resposta);
-      
+        nota1 = Float.parseFloat(resposta);     
         resposta = JOptionPane.showInputDialog("Entre com segunda nota: ");
-        nota2 = Float.parseFloat(resposta);
-        
+        nota2 = Float.parseFloat(resposta);        
         media = (nota1+nota2)/2;
-        JOptionPane.showMessageDialog(null, "Média:" + media, "Question",JOptionPane.QUESTION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Média:" + media, "Question",
+        JOptionPane.QUESTION_MESSAGE);
     }
 }
