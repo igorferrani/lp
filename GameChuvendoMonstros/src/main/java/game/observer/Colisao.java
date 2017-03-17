ab57c3e6333bf80266bd9f1b4abef67b9830066a
felipefo <felipefo@gmail.com>
2017-03-17 17:15:53 -0300

    .
    .

---
+++ GameChuvendoMonstros/src/main/java/game/observer/Colisao.java
@@ -0,0 +1,15 @@
+
+package game.observer;
+
+/**
+ *
+ * @author felip
+ */
+public class Colisao   implements Observador{
+
+    @Override
+    public void update(Observavel ob) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
